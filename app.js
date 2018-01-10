72ababad66aeabfdb88d37aabaa01189d60cf048
sobieszs <sobieszek.s.a@gmail.com>
2018-01-10 02:17:20 +0100

    Added some change

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var morgan = require('morgan');
+var logger = require('morgan');
+
 var app = express();
 
+var routes = require('./routes/index');
+var user - require('./routes/user');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -10,6 +13,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.get('*', function(req, res, next){
 	res.send('This is my default route')
 })
+
+
 
 var server = app.listen(3000, function(){
 	console.log('listening on port', server.address().port)
